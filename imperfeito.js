2d8814fa4331df3f1f24a1f945c2ea0127c0d4a5
Paul-Emmanuel Vandenburie <paulvand@cisco.com>
2014-11-02 23:59:33 +0100

    Moved generic verb functions into verb.js

--- imperfeito.js
+++ imperfeito.js
@@ -2,45 +2,6 @@
 // Portuguese Imperfeito conjuging function
 //
 
-//
-//  Generic verb functions 
-//
-
-function getgroup(verb) {
-	if (verb.slice(verb.length-2) === "ar") {
-			return 1;
-	}
-	else if (verb.slice(verb.length-2) === "er") {
-			return 2;
-	}
-	else if (verb.slice(verb.length-2) === "ir") {
-			return 3;
-	}
-	else {
-			console.log("no group !");
-	}
-}
-
-function radical(verb) {
-	return verb.slice(0, -2);
-}
-
-
-var subjectTerminaisonIndexes = {
-	"eu" : 0,
-	"tu" : 1,
-	"voce" : 2,
-	"ele" : 2,
-	"ela" : 2,
-	"nos" : 3,
-	"voces" : 4,
-	"eles" : 4,
-	"elas" : 4
-}
-
-//
-// Imperfeito functions
-//
 
 // Entry point for imperfeito conjuging
 function noimperfeito(subject, verb) {
---
+++ verb.js
@@ -0,0 +1,35 @@
+//
+//  Generic verb functions 
+//
+
+function getgroup(verb) {
+	if (verb.slice(verb.length-2) === "ar") {
+			return 1;
+	}
+	else if (verb.slice(verb.length-2) === "er") {
+			return 2;
+	}
+	else if (verb.slice(verb.length-2) === "ir") {
+			return 3;
+	}
+	else {
+			console.log("no group !");
+	}
+}
+
+function radical(verb) {
+	return verb.slice(0, -2);
+}
+
+
+var subjectTerminaisonIndexes = {
+	"eu" : 0,
+	"tu" : 1,
+	"voce" : 2,
+	"ele" : 2,
+	"ela" : 2,
+	"nos" : 3,
+	"voces" : 4,
+	"eles" : 4,
+	"elas" : 4
+}
