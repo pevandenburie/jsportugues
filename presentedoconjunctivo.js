d0630501d96252a7f555f1cf12b2b1f68e1d096a
Paul-Emmanuel Vandenburie <pevandenburie@yahoo.fr>
2014-12-20 23:32:10 +0100

    Added irregulares to presente do conjunctivo.

--- presentedoconjunctivo.js
+++ presentedoconjunctivo.js
@@ -7,6 +7,9 @@ function nopresentedoconjunctivo(subject, verb) {
     if (_(presentedoconjunctivoAlteredRadical).has(verb)) {
         return presentedoconjunctivoAlteredRadical[verb] + getpresentedoconjunctivoterminaison(subject, verb);
     }
+    else if (_(presentedoconjunctivoIrregulares).has(verb)) {
+        return (presentedoconjunctivoIrregulares[verb])[ subjectTerminaisonIndexes[subject] ];
+    }
     else {
         return radical(verb) + getpresentedoconjunctivoterminaison(subject, verb);
     }
@@ -28,6 +31,28 @@ var presentedoconjunctivoAlteredRadical =
         "vestir" : "vist"
     }
 
+var presentedoconjunctivoIrregulares =
+    {
+        "dar" : ["dê", "dês", "dê", "dêmos", "deem"],
+        "estar" : ["esteja", "estejas", "esteja", "estejamos", "estejam"],
+        "ter" : ["tenha", "tenhas", "tenha", "tenhamos", "tenham"],
+        "ser" : ["seja", "sejas", "seja", "sejamos", "sejam"],
+        "ir" : ["vá", "vás", "vá", "vamos", "vão"],
+        "ler" : ["leia", "leias", "leia", "leiamos", "leiam"],
+        "fazer" : ["faça", "faças", "faça", "façamos", "façam"],
+        "vir" : ["venha", "venhas", "venha", "venhamos", "venham"],
+        "ver" : ["veja", "vejas", "veja", "vejamos", "vejam"],
+        "sair" : ["saia", "saias", "saia", "saiamos", "saiam"],
+        "pôr" : ["ponha", "ponhas", "ponha", "ponhamos", "ponham"],
+        "poder" : ["possa", "possas", "possa", "possamos", "possam"],
+        "saber" : ["saiba", "saibas", "saiba", "saibamos", "saibam"],
+        "dizer" : ["diga", "digas", "diga", "digamos", "digam"],
+        "trazer" : ["traga", "tragas", "traga", "tragamos", "tragam"],
+        "querer" : ["queira", "queiras", "queira", "queiramos", "queiram"],
+        "haver" : ["", "", "haja", "", ""],
+    }
+        
+        
 var presentedoconjunctivoTerminaisonsGroup1 = ["e", "es", "e", "emos", "em"];
 
 var presentedoconjunctivoTerminaisonsGroup2 = ["a", "as", "a", "amos", "am"];
