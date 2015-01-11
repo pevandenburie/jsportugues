123ef384f2b90f54d173013bcbd2a689692f7df2
Paul-Emmanuel Vandenburie <pevandenburie@yahoo.fr>
2015-01-11 19:40:58 +0100

    Added ex Presente do Conjunctivo.

---
+++ presentedoconjunctivo.exo.js
@@ -0,0 +1,44 @@
+//
+// Portugues Presente do Conjunctivo exercise
+//
+
+var verbList = [
+  "abrir",
+  "beber",
+  "buscar",
+  "cantar",
+  "correr",
+  "cair",
+  "descansar",
+  "falar"
+  ];
+
+var verbs = _.union(_(presentedoconjunctivoAlteredRadical).keys(), _(presentedoconjunctivoIrregulares).keys(), verbList);
+
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function generateRandomSubject() {
+  var subjects = _(subjectTerminaisonIndexes).keys();
+  var idx = getRandomInt(0, _.size(subjects)-1 );
+  return subjects[idx];
+}
+
+function generateRandomVerb() {
+  var idx = getRandomInt(0, _.size(verbs)-1 );
+  return verbs[idx];
+}
+
+// Init of the question
+function createQuestion() {
+  // Select a subject and a verb, and compute the solution
+  var subject = generateRandomSubject();
+  var verb = generateRandomVerb();
+  var solution = nopresentedoconjunctivo(subject, verb);
+  var solutionFullText = subject + " " + solution;
+
+  var questionText = subject + " (" + verb + ")";
+  return { 'subject': subject, 'verb': verb, 'text': questionText, 'solution': solution, 'solutionFullText': solutionFullText };
+}
