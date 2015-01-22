6813984c467dae4588696c292ace60fe9671ea9a
Paul-Emmanuel Vandenburie <pevandenburie@yahoo.fr>
2015-01-22 22:07:04 +0100

    Added irregulares for Imperfeito do Conjunctivo.

--- imperfeitodoconjunctivo.js
+++ imperfeitodoconjunctivo.js
@@ -4,18 +4,37 @@
 
 
 function noimperfeitodoconjunctivo(subject, verb) {
-    /*if (_(presentedoconjunctivoAlteredRadical).has(verb)) {
-        return presentedoconjunctivoAlteredRadical[verb] + getpresentedoconjunctivoterminaison(subject, verb);
-    }
-    else if (_(imperfeitodoconjunctivoIrregulares).has(verb)) {
+    if (_(imperfeitodoconjunctivoIrregulares).has(verb)) {
         return (imperfeitodoconjunctivoIrregulares[verb])[ subjectTerminaisonIndexes[subject] ];
     }
-    else {*/
+    else {
         return radical(verb) + getimperfeitodoconjunctivoterminaison(subject, verb);
-    //}
+    }
 }
 
-        
+
+
+var imperfeitodoconjunctivoIrregulares =
+    {
+        "dar" : ["desse", "desses", "desse", "dessemos", "dessem"],
+        "estar" : ["estivesse", "estivesses", "estivesse", "estivessemos", "estivessem"],
+        "ter" : ["tivesse", "tivesses", "tivesse", "tivessemos", "tivessem"],
+        "ser" : ["fosse", "fosses", "fosse", "fossemos", "fossem"],
+        "ir" : ["fosse", "fosses", "fosse", "fossemos", "fossem"],
+        "ler" : ["lesse", "lesses", "lesse", "lessemos", "lessem"],
+        "fazer" : ["fizesse", "fizesses", "fizesse", "fizessemos", "fizessem"],
+        "vir" : ["viesse", "viesses", "viesse", "viessemos", "viessem"],
+        "ver" : ["visse", "visses", "visse", "vissemos", "vissem"],
+        "sair" : ["saisse", "saisses", "saisse", "saissemos", "saissem"],
+        "pôr" : ["pusesse", "pusesses", "pusesse", "pusessemos", "pusessem"],
+        "poder" : ["pudesse", "pudesses", "pudesse", "pudessemos", "pudessem"],
+        "saber" : ["soubesse", "soubesses", "soubesse", "soubessemos", "soubessem"],
+        "dizer" : ["disesse", "disesses", "disesse", "disessemos", "disessem"],
+        "trazer" : ["trouxesse", "trouxesses", "trouxesse", "trouxessemos", "trouxessem"],
+        "querer" : ["quisesse", "quisesses", "quisesse", "quisessemos", "quisessem"],
+    }
+
+
 var imperfeitodoconjunctivoTerminaisonsGroup1 = ["asse", "asses", "asse", "ássemos", "assem"];
 
 var imperfeitodoconjunctivoTerminaisonsGroup2 = ["esse", "esses", "esse", "êssemos", "essem"];
