317aa5ee0b45d1b072b0b606a309111c9e08bcb5
Paul-Emmanuel Vandenburie <pevandenburie@yahoo.fr>
2015-11-12 23:09:18 +0100

    Added Backbone.js, and started the log as a View/Model (not functionnal now).

--- log.js
+++ log.js
@@ -2,9 +2,64 @@
 // Logger element
 //
 
-function log_insert( id, isSuccess, useranswer,  solutionFullText)
+
+var AnswerLogItem = Backbone.Model.extend({
+	urlRoot: 'log',
+	defaults: {
+		userAnswer: "NA",
+		isSuccess: false,
+		solutionFullText: "NA"
+	}
+});
+
+
+
+var AnswerLogView = Backbone.View.extend({
+	id: 'answerslog-view',
+	className: 'log',
+
+	template: _.template("<div><%= userAnswer %> <% if (isSuccess) { print('Correto'); } else { print('Falso'); } > (<%= solutionFullText %>)</div>"),
+
+	render: function() {
+		/*if (this.model.get('isSuccess')) {
+			var html = '<div>' + this.model.get('useranswer') + ' Correto!  (' + this.model.get('solutionFullText') + ')' + '</div>';	
+		}
+		else  {
+			var html = '<div>' + this.model.get('useranswer') + ' Falso!  (' + this.model.get('solutionFullText') + ')' + '</div>';	
+		}
+		this.$el.html(html);*/
+
+		var attributes = this.model.toJSON();
+		this.$el.html(this.template(attributes));
+	}
+});
+
+
+
+
+function log_insert( id, isSuccess, userAnswer,  solutionFullText)
 {
-	var logList = document.getElementById( id );
+	// Create a new log entry
+	var answerLogItem = new AnswerLogItem();
+	answerLogItem.set({
+		userAnswer: this.userAnswer, 
+		isSuccess: this.isSuccess,
+		solutionFullText: this.solutionFullText 
+	});
+
+	//answerLogItem.save();
+	//answerLogItem.fetch();
+
+
+	var answerLogView = new AnswerLogView({
+		model: answerLogItem,
+	});
+
+	answerLogView.render();
+
+
+
+	/*var logList = document.getElementById( id );
 
 	var logEntry = document.createElement("DIV");
 	if (isSuccess) {
@@ -17,5 +72,5 @@ function log_insert( id, isSuccess, useranswer,  solutionFullText)
 		logEntry.setAttribute("class", "wrong");
 		logEntry.textContent = "\"" + useranswer + "\"" + " Falso!  (" + solutionFullText + ")";
 	}
-	logList.insertBefore(logEntry, logList.firstChild);
+	logList.insertBefore(logEntry, logList.firstChild);*/
 }
