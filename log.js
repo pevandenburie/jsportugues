325e2502e6730ec69c983acd328c0bfef2fe1d1f
Paul-Emmanuel Vandenburie <pevandenburie@yahoo.fr>
2016-01-15 23:18:43 +0100

    Backbone: exo imperfeito includes the answerLogView and model.

--- log.js
+++ log.js
@@ -72,28 +72,6 @@ function log_insert( id, isSuccess, userAnswer,  solutionFullText)
 		isSuccess: isSuccess,
 		solutionFullText: solutionFullText
 	});
-	
+
 	logList.add(answerLogItem);
-
-	//answerLogItem.save();
-	//answerLogItem.fetch();
-
-
-
-
-
-	/*var logList = document.getElementById( id );
-
-	var logEntry = document.createElement("DIV");
-	if (isSuccess) {
-		score.nbSuccess++;
-		logEntry.setAttribute("class", "right");
-		logEntry.textContent = "\"" + useranswer + "\"" + " Correto!  (" + solutionFullText + ")";
-	}
-	else {
-		score.nbFailure++;
-		logEntry.setAttribute("class", "wrong");
-		logEntry.textContent = "\"" + useranswer + "\"" + " Falso!  (" + solutionFullText + ")";
-	}
-	logList.insertBefore(logEntry, logList.firstChild);*/
 }
